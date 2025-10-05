6943e889690d1e01545dcd5930ecfa2af5f443ec
fatimahawny <fatimahawny@gmail.com>
2025-10-05 22:12:28 +0300

    add app logo

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+const kPrimaryColor = Color(0xFFfcb4ab);
+const kSecondaryColor = Color(0xFFfcd8d3);
+const kLightColor = Color(0xFFfcf8eb);
+const kFontsColor = Color(0xFF10183d);
+const kButtonsColor = Color(0xFFea5f1c);
+const kAppLogo = "assets/images/fashion_app_logo.png";
---
+++ lib/pages/signin_page.dart
@@ -0,0 +1,14 @@
+import 'package:fashion_app/constants.dart';
+import 'package:flutter/material.dart';
+class SigninPage extends StatelessWidget {
+  const SigninPage({super.key});
+static String id ="signin";
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+
+      body: Center(
+      )
+    );
+  }
+}
