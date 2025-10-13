7000f565a9592a73cf3115c187d6996de8c44c83
fatimahawny <fatimahawny@gmail.com>
2025-10-13 11:53:42 +0300

    post method

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
@@ -10,6 +11,25 @@ class Api {
       throw Exception(
         'there is an error with status code ${response.statusCode}',
       );
+    }
+  }
+
+  Future<dynamic> post({
+    required String url,
+    @required dynamic body,
+    @required String? token,
+  }) async {
+    Map<String, String> Headers = {};
+    if (token == null) {
+      Headers.addAll({'Authorization': 'Bearer $token '});
+    } else {
+      Headers.addAll(Headers);
+
+      http.Response response = await http.post(Uri.parse(url), body: body,
+      headers: Headers);
+      Map<String, dynamic> data = jsonDecode(response.body);
+        return data;
+
     }
   }
 }
